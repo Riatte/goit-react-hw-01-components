fff2bfc313b3750b1503c872717aad95c6ba8870
Varvara Babych <varvara.babich13@gmail.com>
2023-04-20 21:05:15 +0300

    profile+statistics+friends

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,12 @@
+import user from '../components/Profile/user.json';
+import data from '../components/Statistics/data.json';
+import friends from '../components/Friends/friends.json';
+
+
+import { Profile } from './Profile/Profile';
+import { Statistics } from './Statistics/Statistics';
+import {FriendList} from './Friends/FriendList'
+
 export const App = () => {
   return (
     <div
@@ -7,10 +16,20 @@ export const App = () => {
         justifyContent: 'center',
         alignItems: 'center',
         fontSize: 40,
-        color: '#010101'
+        color: '#010101',
+        flexDirection: 'column',
+        marginTop: '20px'
       }}
     >
-      React homework template
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+      <Statistics title="Upload stats" stats={data} />
+      <FriendList friends={friends} />
     </div>
   );
 };
---
+++ src/components/Statistics/Statistics.jsx
@@ -0,0 +1,27 @@
+import PropTypes from "prop-types";
+import css from "./Statistics.module.css"
+
+export const Statistics = ({ title, stats }) => (
+    <section className={css.statistics}>
+        {title && <h2 class="title">Upload stats</h2>}
+        
+         <ul className={css.statList}>
+            {stats.map(({ id, label, percentage }) => (<li key={id} className={css.item}>
+                <span className={css.label}>{label}</span>
+                <span className={css.percentage}>{percentage}%</span>
+            </li>)
+            )};
+        </ul>
+    </section>
+);
+
+
+Statistics.propTypes = {
+    title: PropTypes.string,
+    stats: PropTypes.arrayOf(PropTypes.exact
+        ({
+            id: PropTypes.string.isRequired,
+            label: PropTypes.string.isRequired,
+            percentage: PropTypes.number.isRequired,
+    }))
+}
